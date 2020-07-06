611b92c8f607b82f170400a55930f90084f4c83a
nilsobi10 <67693844+nilsobi10@users.noreply.github.com>
2020-07-06 15:29:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var person = prompt("Please enter your name", "Harry Potter");
 
-alert("hi")
-console.log("5")
+if (person == null || person == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  txt = "Hello " + person + "! How are you today?";
+}
