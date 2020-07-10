e1993f94d8bf648790c5fffd11488e0244e890e6
nilsobi10 <nils.obi10@gmail.com>
2020-07-10 22:25:14 +0200

    Fragebogen

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ var person = prompt("Geben Sie bitte ihren Namen ein!");
 
 if (person == null || person == "") {
   txt = "Der Nutzer hat keine Eingabe eingegeben";
-} else { alert( "Hallo " + person + "! Wie geht es Ihnen heute??");
-}
+} else {alert( "Hallo " + person + "! ")} ;
+
+
+
+var argument = prompt("Wie geht es Ihnen? ");
+
+if (argument == null || argument == "") {
+  txt = "Der Nutzer " + person +" hat keine Eingabe eingegeben! ";
+} else {alert( person + "! Einen schönen Tag noch! ")} ;
