5981c85173f03df4a523cfcb519dbe038d680a92
nilsobi10 <67693844+nilsobi10@users.noreply.github.com>
2020-07-15 08:34:38 +0200

    Update daten.js

--- daten.js
+++ daten.js
@@ -1,12 +1,4 @@
 	document.querySelector('#textarea').onclick = function () {
 		document.querySelector('.textarea-msg').innerHTML = document.querySelector("#justtext").value;
 	}
-        
-
-    
-
-
-
-
-<input type="text" name="vorname" id="vorname">
- let msg = document.querySelector("#vorname").value;
+  
