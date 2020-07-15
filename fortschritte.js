f37173c962434f70861a337e9da7c155e91dfca5
nilsobi10 <67693844+nilsobi10@users.noreply.github.com>
2020-07-15 09:08:40 +0200

    Update fortschritte.js

--- fortschritte.js
+++ fortschritte.js
@@ -1,12 +1,23 @@
 var person = prompt("Geben Sie bitte ihren Namen ein!");
 
-if (person == null || person == "" || person <= 13 || person > 13) {
-  alert("Der Nutzer hat keine Eingabe eingegeben") ;
-} else {alert( "Hallo " + person + "! ")} ;
+if (person == null || person == "" || person <= 13 || person > 13) 
+  {
+     alert("Der Nutzer hat keine Eingabe eingegeben") ;
+  }
+
+else {
+   alert( "Hallo " + person + "! ")
+  };
 
 
 
 var argument = prompt("Wie geht es Ihnen? ");
 
-if (argument == null || argument == "" || argument <= 13 || argument  > 13){alert( "Der Nutzer " + person +" hat keine Eingabe eingegeben! ")}
-else{alert("Info: Ihr Name lautet " + person +" und Ihnen geht es "+ argument+ " !")} ;
+if (argument == null || argument == "" || argument <= 13 || argument  > 13)
+  {
+    alert( "Der Nutzer " + person +" hat keine Eingabe eingegeben! ")
+  }
+
+else{
+    alert("Info: Ihr Name lautet " + person +" und Ihnen geht es "+ argument+ " !")
+  };
