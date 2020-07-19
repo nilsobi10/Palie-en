b6baf425b70dbd930a035790fd963823b5c085e2
nilsobi10 <nils.obi10@gmail.com>
2020-07-19 18:30:25 +0200

    Hallo

--- fortschritte.js
+++ fortschritte.js
@@ -1,6 +1,7 @@
 var person = prompt("Geben Sie bitte ihren Namen ein!");
 
-if (person == null || person == "" || person <= 13 || person > 13) 
+
+ if (person == null || person == "" || person <= 13 || person > 13) 
   {
      alert("Der Nutzer hat keine Eingabe eingegeben") ;
   }
@@ -11,13 +12,20 @@ else {
 
 
 
-var argument = prompt("Wie geht es Ihnen? ");
+var argument = prompt("Wie geht es Ihnen? ");!
 
-if (argument == null || argument == "" || argument <= 13 || argument  > 13)
-  {
-    alert( "Der Nutzer " + person +" hat keine Eingabe eingegeben! ")
-  }
 
-else{
-    alert("Info: Ihr Name lautet " + person +" und Ihnen geht es "+ argument+ " !")
+
+
+if 
+
+
+(argument== !"")
+{alert("Keine Eingabe");
+  
+}
+    
+    
+    
+    else{alert("Info: Ihr Name lautet " + person +" und Ihnen geht es "+ argument+ " !" )
   };
