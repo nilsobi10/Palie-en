2ac5812eb5b8ae084b2e737d25e0d38cdcaa1b49
nilsobi10 <nils.obi10@gmail.com>
2020-07-24 23:25:49 +0200

    Neu

--- daten.js
+++ daten.js
@@ -1,7 +1,12 @@
-var h = prompt("Geben Sie bitte Ihre Höhe in Zentimeter ein"),
+var h = prompt("Geben Sie bitte Ihre Höhe in Zentimeter ein");
+
+if(h>=0){
 r = 506.25
 v = Math.PI*(h/10)*r, 
 l = v,
 p = 12,       // Leistung pro Minute
 t = l/p;
-alert("Die Befüllung dauert "+t+" Minuten. ")
+
+document.write("Ihre Befüllung von "+h+" Zentimeter dauert "+((t).toFixed(1)) + " Minuten. ")}
+
+else{alert("Keine gültige Eingabe")} 
--- fortschritte.js
+++ fortschritte.js
@@ -4,21 +4,19 @@ var person = prompt("Geben Sie bitte ihren Namen ein!");
  if (person == null || person == "" || person <= 13 || person > 13) 
   {
      alert("Der Nutzer hat keine Eingabe eingegeben") ;
+     var argument = prompt("Wie geht es dir ?");
+ 
   }
 
 else {
    alert( "Hallo " + person + "! ")
-  };
-
-
-
-var argument = prompt("Wie geht es dir, " +person+ " ?");
+var argument = prompt("Wie geht es dir, " +person+ " ?");} ;
 
 
 
 if (person == null || person == "" || person <= 13 || person > 13)
 {
-  alert("Dem Nutzer geht es "+argument+" "+"!");
+  
 }
 
 else{if(argument== null || argument == ""){alert("Der Nutzer hat kein Eingabe eingegeben! " )} 
